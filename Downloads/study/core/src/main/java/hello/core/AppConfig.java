a1b052d06b45b015a199d89592b0813f129ba9b4
sejin\admin <seijin0722@naver.com>
2023-04-16 15:52:56 +0900

    새로운 구조와 할인 정책 적용

--- Downloads/study/core/src/main/java/hello/core/AppConfig.java
+++ Downloads/study/core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDisocuntPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -12,7 +13,7 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     // 생성자 주입 : 생성자를 통해 객체가 들어간다.
-    // 역할과 그 역할에 대한 구현이 한 눈에 구분된다, 중복되 제거됨 : 애플리케이션 구성이 어떻게 되어있는지 빠르게 파악 가능
+    // 역할과 그 역할에 대한 구현이 한 눈에 구분된다, 중복이 제거됨 : 애플리케이션 구성이 어떻게 되어있는지 빠르게 파악 가능
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
@@ -26,6 +27,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDisocuntPolicy();
+//        return new FixDisocuntPolicy();
+        return new RateDiscountPolicy();
     }
 }
